22355a7b459f3e3d75b53445ab0c6d55b496b56b
frx <sabado.fredey@gmail.com>
2020-04-02 23:14:53 +0800

    30. Card component

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Card } from '../card/card.component'
+import './card-list.styles.css'
+
+export const CardList = props => {
+    console.log('@CardList: props: ', props)
+    return <div className='card-list'>
+        { props.monsters.map(m => 
+            <Card key={m.id} monster={m}/>) 
+        }
+    </div>
+    
+}
+export default CardList
