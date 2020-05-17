07644e4c73bf8815c107c58d155004c51ca3e356
frx <sabado.fredey@gmail.com>
2020-05-17 14:54:06 +0800

    Updates

---
+++ precache-manifest.dbb668c4ea6410894a690c2ad299e690.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6599904c7308a11cc7be97d29c09ae6d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6554fccb247f1dc6b3a8",
+    "url": "/monsters-rolodex/static/css/main.bca88ed8.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6554fccb247f1dc6b3a8",
+    "url": "/monsters-rolodex/static/js/main.d24597fb.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
